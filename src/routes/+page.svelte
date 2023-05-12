f8e9fc4ce6114a2d2e68418f770236d04ffe97a9
LolzD10 <LolzD10@hotmail.com>
2023-05-13 00:25:23 +0200

    /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
-<script>
-	import Button, { Label } from '@smui/button';
-</script>
+<script lang="ts">
+    import Fab, { Icon } from '@smui/fab';
+  </script>
 
 
 <h1>Welcome to AereoArcade</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 
-<Button default href="/games" >
-    <Label>
-        Games
-    </Label>
-</Button>
+<div class="flexy">
+    <div class="margins">
+      <Fab href="/games">
+        <Icon class="material-icons">favorite</Icon>
+      </Fab>
+    </div>
+  </div>
 
 <style>
 
