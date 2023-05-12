3dcb0aa0a36f8ad3e9e3f5ebe3531431354ae502
LolzD10 <LolzD10@hotmail.com>
2023-05-13 00:12:36 +0200

    /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 
-<Button href="/games">
+<Button default href="/games" >
     <Label>
         Games
     </Label>
