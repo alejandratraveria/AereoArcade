cb8eb3bb6245e0849ab13b931571183c67435ee4
LolzD10 <LolzD10@hotmail.com>
2023-05-13 00:03:56 +0200

    /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+	import Button, { Label } from '@smui/button';
+</script>
+
+
 <h1>Welcome to AereoArcade</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Button href="/games">
+    <Label>
+        Games
+    </Label>
+</Button>
+
+<style>
+
+</style>
---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Button, { Label } from "@smui/button";
+</script>
+
+<Button>
+    <Label>
+        Atras
+    </Label>
+</Button>
+
+<h1>this is game</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
