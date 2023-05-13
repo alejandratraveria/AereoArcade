c14b05e274b497bd20efda64eb2a89e2e0b21d0b
LolzD10 <LolzD10@hotmail.com>
2023-05-13 03:42:45 +0200

    /

--- src/routes/gamesHub/+page.svelte
+++ src/routes/gamesHub/+page.svelte
@@ -1,1 +1,26 @@
-<p>Mi primera página web</p>
+
+<div>
+    <h1>Singleplayer</h1> 
+</div>
+<LayoutGrid>
+    {#each Array(2) as _unused, i}
+      <Cell>
+        <div class="demo-cell">Game {i + 1}</div>
+      </Cell>
+    {/each}
+  </LayoutGrid>
+   
+  <script lang="ts">
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
+  </script>
+   
+  <style>
+    .demo-cell {
+      height:   60px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: var(--mdc-theme-secondary, #333);
+      color: var(--mdc-theme-on-secondary, #fff);
+    }
+  </style>
