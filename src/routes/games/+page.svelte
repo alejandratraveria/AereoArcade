75924aec5f0bae11addb9977eb7cb613422df809
Teixi09 <eteixido0904@gmail.com>
2023-05-13 06:23:55 +0200

    .

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,12 +1,39 @@
-<script lang="ts">
-    import Button, { Label } from "@smui/button";
-</script>
+<div class="title">
+    <h1>Singleplayer</h1> 
+</div>
 
-<Button href="/">
-    <Label>
-        Atras
-    </Label>
-</Button>
+    <div class="ButtonContainer">
+        {#each Array(8) as _unused, i}
+        <Button class="lbutton">
+            <Label>Game {i + 1}</Label>
+        </Button>
+        {/each}
+    </div>
 
-<h1>Games</h1>
+  <script lang="ts">
+    import Button, {Label} from '@smui/button';
+  </script>
+   
+  <style>
 
+    * :global(.lbutton) {
+        width: 250px;
+        height: 250px;
+        display: flex;
+        align-items: center;  
+        background-color: var(--mdc-theme-secondary, #333);
+        color: var(--mdc-theme-on-secondary, #fff);
+    }
+
+    .ButtonContainer {
+
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
+
+    .title { 
+        background-color: yellow;
+        padding: 5px;
+    }
+  </style>
