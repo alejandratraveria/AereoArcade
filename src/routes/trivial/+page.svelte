1b062f1458b58970b5c1b3642615c39d442a2a64
Teixi09 <eteixido0904@gmail.com>
2023-05-13 23:30:11 +0200

    trivial demo finished

--- src/routes/trivial/+page.svelte
+++ src/routes/trivial/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	let count = 0;
+    let points = 0;
 	
 	function handleClick() {
 		count += 1;
@@ -11,25 +12,55 @@
     <div class="menu">
         <ul>
             <!-- svelte-ignore a11y-missing-attribute -->
-            <li><img class=logo src="Logo_Vueling.png"/></li>
-            <li><b>Points: </b></li>
-            <li>a</li>
+            <li><img class="logo" src="Logo_Vueling.png"/></li>
+            <li><b>Points:</b></li>
+            <li>{points}</li>
             <li class="rigthb"><a class="mbutton" href="/">Back</a></li>
         </ul>
     </div>
+
+    <div class="question">
+        ¿Cual es la capital de España?
+    </div>
+
+    <ul>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <li>
+            <button on:click={() => points -= 100}>
+                Lisboa
+            </button>
+        </li>
+        <li>
+            <button on:click={() => points -= 100}>
+                Barcelona
+            </button>
+        </li>
+        <li>
+            <button on:click={() => points -= 100}>
+                Estocolmo
+            </button>
+        </li>
+        <li>
+            <button on:click={() => points += 200}>
+                Madrid
+            </button>
+        </li>
+    </ul>
 </div>
 
 
 <style>
-
-
-    
     *{
         padding: 0;
         margin: 0;
         font-family: sans-serif;
     }
-
+    li {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 50px;
+    }
     .container {
         width: 100%;
         height: 100%;
@@ -54,7 +85,13 @@
     .rigthb{
         padding-left: 900px;
     }
-
+    .question {
+        font-size: XXX-large;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 50px;
+    }
     .mbutton{
         -webkit-appearance: button;
         -moz-appearance: button;
