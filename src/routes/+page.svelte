1e472345c17d6e0b6dd332cb83b002359cfde843
Teixi09 <eteixido0904@gmail.com>
2023-05-13 04:46:35 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,49 @@
 </script>
 
 <main>
-    <div>
+    <div class="header">
         <h1><img class="gamesImg" src="AereoArcadeIcon.png"/></h1>
     </div>
-    <Button default href="/games" >
-        <Label>SinglePlayer</Label>
-    </Button>
 
-    <Button default href="/games" >
-        <Label>MultiPlayer</Label>
-    </Button>
+    <div>
+        <Button default href="/games">
+            <Label>SinglePlayer</Label>
+        </Button>
 
+        <Button default href="/games">
+            <Label>MultiPlayer</Label>
+        </Button>
+    </div>
+
+    <div>
+        <Button>
+            <Label>Trivial</Label>
+        </Button>
+
+        <Button>
+            <Label>Stamps</Label>
+        </Button>
+    </div>
 
 </main>
 
 <style>
+    .header {
+        
+    }
     .gamesImg {
-        width: 150px;
-        aspect-ratio: 2;
+        width: 450px;
+        aspect-ratio: 3;
+    }
+    h1 {
+        margin: 0;
     }
     main {
         display:flex;
         flex-direction: column;
+    }
+    div {
+        display: flex;
+        align-items: center;
     }
 </style>
