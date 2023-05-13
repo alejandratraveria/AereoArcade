9cdc60a32848e980ac9eef196ef37fe575f08a26
Teixi09 <eteixido0904@gmail.com>
2023-05-13 03:00:25 +0200

    Main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,28 @@
 	import Button, { Label } from '@smui/button';
 </script>
 
+<main>
+    <div>
+        <h1><img class="gamesImg" src="AereoArcadeIcon.png"/></h1>
+    </div>
+    <Button default href="/games" >
+        <Label>SinglePlayer</Label>
+    </Button>
 
-<h1>Welcome to AereoArcade</h1>
+    <Button default href="/games" >
+        <Label>MultiPlayer</Label>
+    </Button>
 
 
-<Button default href="/games" >
-    <Label>
-        Games
-    </Label>
-</Button>
+</main>
 
 <style>
-
+    .gamesImg {
+        width: 150px;
+        aspect-ratio: 2;
+    }
+    main {
+        display:flex;
+        flex-direction: column;
+    }
 </style>
